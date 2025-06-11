73af888ae5af6bbec557a68df4fad482b1eadb7c
William Parlow <williamparlow@gmail.com>
2025-06-11 00:23:51 -0300

    fixed activity code

--- R/conceitos_basicos/atividade_3_nota-USArrests.R
+++ R/conceitos_basicos/atividade_3_nota-USArrests.R
@@ -1,22 +1,17 @@
 data(USArrests)
 
+# Vetores auxiliares para ordenação
 aux_max = c(-1,-1,-1,-1,-1)
 aux_min = c(-1,-1,-1,-1,-1)
 
+# Estrutura de repetição para efetuar a ordenação das cidades mais populosas e
+# menos populosas
 for (i1 in 1:5) {
   for (i2 in 1:50) {
     if (USArrests$UrbanPop[i2] != 0 & aux_min[i1] == -1) {
       aux_min[i1] = i2
-      print("----")
-      print(i1)
-      print(i2)
-      print("----")
     } else if (USArrests$UrbanPop[i2] != 0 & USArrests$UrbanPop[aux_min[i1]] > USArrests$UrbanPop[i2]) {
       aux_min[i1] = i2
-      print("----")
-      print(i1)
-      print(i2)
-      print("----")
     }
     if (USArrests$UrbanPop[i2] != 0 & aux_max[i1] == -1) {
       aux_max[i1] = i2
@@ -28,6 +23,8 @@ for (i1 in 1:5) {
   USArrests$UrbanPop[aux_max[i1]] = 0
 }
 
+# Realiza o print das médias de assassinatos, assalto e estupro para as cidades 
+# de maior população
 assassinato = 0
 assalto = 0
 estupro = 0
@@ -42,6 +39,8 @@ cat("Assassinato: ", assassinato / 5)
 cat("Assalto: ", assalto / 5)
 cat("Estupro: ", estupro / 5)
 
+# Realiza o print das médias de assassinatos, assalto e estupro para as cidades 
+# de menor população
 assassinato = 0
 assalto = 0
 estupro = 0
