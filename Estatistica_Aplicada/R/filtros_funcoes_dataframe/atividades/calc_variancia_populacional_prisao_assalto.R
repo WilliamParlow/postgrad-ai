1b4f27223dd655a079bac91de5ac9860de74c5e1
William Parlow <williamparlow@gmail.com>
2025-07-06 12:37:27 -0300

    Removed code from R as it is insignificant for my purposes

--- Estatistica_Aplicada/R/filtros_funcoes_dataframe/atividades/calc_variancia_populacional_prisao_assalto.R
+++
@@ -1,3 +0,0 @@
-# Implementar um script usando a linguagem R para calcular por meio 
-# de uma função a variância populacional para o número de prisões por 
-# assaltos do USArrests
--- Estatistica_Aplicada/R/filtros_funcoes_dataframe/atividades/usarest_med_mediana_var_dp_populacao.R
+++
@@ -1,3 +0,0 @@
-# Implementar um script em linguagem R para calcular a média, a 
-# mediana, a variância e o desvio-padrão das populações das linhas 
-# pares do USArrests
